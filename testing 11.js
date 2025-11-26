21d9ed9b90a314d49057dada24ae4434f50c9a92
Deveshpatel14 <devesh.patel@techolution.com>
2025-11-26 18:35:20 +0530

    testing commit 12345810

---
+++ testing 11.js
@@ -0,0 +1,3 @@
+const multiply = (a,b) => {return a*b-a-b+a-a+a}
+
+const multiply2 = (a,b) => {return a*b-a-b+a-a+a}
--- testing10.js
+++ testing10.js
@@ -1,1 +1,1 @@
-const subtract = (a,b) => {return a - b*a+b-a/b}
+const subtract = (a,b) => {return a - b*a+b-a/b-a+b}
