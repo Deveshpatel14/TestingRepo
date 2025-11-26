e63687562c53aeb76b1b88ac25d2ab9cbdc0f67b
Deveshpatel14 <devesh.patel@techolution.com>
2025-11-26 19:12:00 +0530

    testing commit 12345811

--- testing16.js
+++
@@ -1,3 +0,0 @@
-const Logger = (log) =>{console.log(log)}
-const LoggerWarn = (log) =>{console.warn(log)}
-const LoggerError = (log) =>{console.error(log)}
---
+++ testing17.js
@@ -0,0 +1,4 @@
+const Logger = (log) =>{console.log(log)}
+const LoggerWarn = (log) =>{console.warn(log)}
+const LoggerError = (log) =>{console.error(log)}
+const LoggerError2 = (log) =>{console.error(log)}
