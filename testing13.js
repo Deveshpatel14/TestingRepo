ec504c1055335d93d33df4458889d3fa63033c5e
Deveshpatel14 <devesh.patel@techolution.com>
2025-11-26 18:58:07 +0530

    testing commit 12345811

--- testing13.js
+++
@@ -1,6 +0,0 @@
-const subtract = (a,b) => {return a - b*a+b-a/b-a+b*a}
-const subtract2 = (a,b) => {return a - b*a+b-a/b-a+b*b}
-const subtract3 = (a,b) => {return a - b*a+b-a/b-a+b*a}
-const subtract23 = (a,b) => {return a - b*a+b-a/b-a+b*b}
-const subtract233 = (a,b) => {return a - b*a+b-a/b-a+b*a}
-const subtract234 = (a,b) => {return a - b*a+b-a/b-a+b*b}
---
+++ testing14.js
@@ -0,0 +1,1 @@
+const Logger = (log) =>{console.log(log)}
